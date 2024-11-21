771c2ed76461d91d4d9056c7b5f2649bce4df170
KhrustDanil <danil.khrust29@gmail.com>
2024-11-21 12:03:07 +0200

    fixed

--- swagger-ui-init.js
+++ swagger-ui-init.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const ui = SwaggerUIBundle({
-      url: "http://localhost:3000/api-docs", // Адреса JSON-документації
+      url: "http://localhost:3000/api-docs",
       dom_id: '#swagger-ui',
       presets: [
         SwaggerUIBundle.presets.apis,
